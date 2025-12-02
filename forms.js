ffd381125ab9079aaff250969f8e97dbe56c4b64
MariMeMotta <memamotta777@gmail.com>
2025-12-02 13:34:14 -0300

    transição e biblioteca J.query para o formulário

---
+++ forms.js
@@ -0,0 +1,9 @@
+
+    function esconderTodos() {
+     $("#form_cliente").hide(3000);
+   $("#form_usuario").hide(3000);
+    }
+    function mostrarTodos() {
+      $("#form_usuario").show(3000);
+     $("#form_cliente").show(3000);
+    }
