f81b98b2be0a867f93f983326578fddb79f96412
proglogi <nichollygonzaga@hotmail.com>
2025-12-02 19:27:11 -0300

    Removendo redundância

--- forms.js
+++
@@ -1,9 +0,0 @@
-
-    function esconderTodos() {
-     $("#form_cliente").hide(3000);
-   $("#form_usuario").hide(3000);
-    }
-    function mostrarTodos() {
-      $("#form_usuario").show(3000);
-     $("#form_cliente").show(3000);
-    }
